d00b28590052ee2595767f43bbebba3c4cd1d585
牛肉拌面 <hardews@qq.com>
2023-10-09 17:18:37 +0800

    update exec func

--- api/shell.go
+++ api/shell.go
@@ -14,6 +14,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"webhook/model"
 	"webhook/util"
 )
@@ -50,30 +51,75 @@ func shellBuild(ctx *gin.Context) {
 		path := os.Getenv(projectPath) + res.Repository.Name
 		sn := os.Getenv(shellName)
 
-		// git pull
-		cmd := exec.Command("", "", fmt.Sprintf("cd %s && git pull", path))
-		if output, err = cmd.Output(); err != nil {
-			log.Printf("git pull error, err: %s", err)
-			ctx.Abort()
+		_, err = os.Stat("./git.sh")
+		if os.IsExist(err) {
+			os.Remove("./git.sh")
+		}
+
+		var file *os.File
+		file, err = os.OpenFile("./git.sh", os.O_CREATE, os.ModePerm)
+		if err != nil {
+			log.Println("open git.sh failed,err:", err)
 			return
 		}
 
-		log.Println("git pull result")
-		fmt.Println(string(output))
+		file.Write([]byte(fmt.Sprintf("cd %s && git pull", path)))
+		file.Write([]byte(fmt.Sprintf("./%s", sn)))
+		file.Close()
 
-		// 开始执行脚本
-		cmd = exec.Command("sh", "-c", fmt.Sprintf("cd %s && ./%s", path, sn))
-		if output, err = cmd.Output(); err != nil {
-			log.Println("build error, err:", err)
-			ctx.Abort()
+		// git pull && 执行脚本
+		cmd := exec.Command("sh", "-c", "./git.sh")
+		stdout, _ := cmd.StdoutPipe()
+		stderr, _ := cmd.StderrPipe()
+
+		err = cmd.Start()
+		if err != nil {
+			log.Println("starting command failed, err:", err)
 			return
 		}
 
-		log.Println("build result")
+		go asyncLog(stdout)
+		go asyncLog(stderr)
+
+		defer func() {
+			err1 := recover()
+			if err1 != nil {
+				panic(err1)
+			}
+		}()
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Println("waiting for command execution failed, err:", err)
+			return
+		}
+
+		log.Println("successful result")
 		fmt.Println(string(output))
 
 		log.Println("build successful!")
 		return
 	}
 	log.Println("signature error!")
+}
+
+func asyncLog(reader io.ReadCloser) error {
+	cache := ""
+	buf := make([]byte, 1024, 1024)
+	for {
+		num, err := reader.Read(buf)
+		if err != nil {
+			if err == io.EOF || strings.Contains(err.Error(), "closed") {
+				err = nil
+			}
+			return err
+		}
+		if num > 0 {
+			oByte := buf[:num]
+			oSlice := strings.Split(string(oByte), "\n")
+			line := strings.Join(oSlice[:len(oSlice)-1], "\n")
+			fmt.Printf("%s%s\n", cache, line)
+			cache = oSlice[len(oSlice)-1]
+		}
+	}
 }
